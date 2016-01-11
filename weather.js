731436c46364d8699034d2fc62f04efc44dd9f33
Todd Jones <atoddj@gmail.com>
2016-01-11 05:45:19 +0000

    Added geolocation code

---
+++ weather.js
@@ -0,0 +1,7 @@
+// Adding geolocation information
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+    });
+}
