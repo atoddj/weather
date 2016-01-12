edef2812cb2b2e07abcf768ae3c461272a368311
Todd Jones <atoddj@gmail.com>
2016-01-12 08:18:10 +0000

    Updated the location to use google maps api

--- weather.js
+++ weather.js
@@ -1,9 +1,52 @@
 // Adding geolocation information
 
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    });
-}
+function displayLocation(latitude,longitude){
+        var request = new XMLHttpRequest();
 
-//test comment
+        var method = 'GET';
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+        var async = true;
+
+        request.open(method, url, async);
+        request.onreadystatechange = function(){
+          if(request.readyState == 4 && request.status == 200){
+            var data = JSON.parse(request.responseText);
+            var address = data.results[0];
+            var city = data.results[0].address_components[2].long_name;
+            var state = data.results[0].address_components[5].short_name;
+            $("#location").html(city+", "+state);
+          }
+        };
+        request.send();
+      };
+
+      var successCallback = function(position){
+        var x = position.coords.latitude;
+        var y = position.coords.longitude;
+        displayLocation(x,y);
+      };
+
+      var errorCallback = function(error){
+        var errorMessage = 'Unknown error';
+        switch(error.code) {
+          case 1:
+            errorMessage = 'Permission denied';
+            break;
+          case 2:
+            errorMessage = 'Position unavailable';
+            break;
+          case 3:
+            errorMessage = 'Timeout';
+            break;
+        }
+        document.write(errorMessage);
+      };
+
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 1000,
+        maximumAge: 0
+      };
+
+      navigator.geolocation.getCurrentPosition(successCallback,errorCallback,options);
+
