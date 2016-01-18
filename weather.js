985ad8de99abf799d3a99ce529b3c9fcd52eb49f
Todd Jones <atoddj@gmail.com>
2016-01-18 15:19:48 +0000

    Updated script src and added comments for clarity

--- weather.js
+++ weather.js
@@ -1,52 +1,51 @@
-// Adding geolocation information
-
 function displayLocation(latitude,longitude){
-        var request = new XMLHttpRequest();
+  // This function uses the googleapis to turn latitude and longitude into city, state format
+    var request = new XMLHttpRequest();
+    
+    var method = 'GET';
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+    var async = true;
+    
+    request.open(method, url, async);
+    request.onreadystatechange = function(){
+      if(request.readyState == 4 && request.status == 200){
+        var data = JSON.parse(request.responseText);
+        var city = data.results[0].address_components[2].long_name;// gets the long version of the city name
+        // May need another variable here for the 5 digit zip code for weather api? 
+        var state = data.results[0].address_components[5].short_name; //gets the 2 letter abbreviation of the current state
+        $("#location").html(city+", "+state);  //sets the inner text of the div with ID of location
+      }
+    };
+    request.send();
+}
 
-        var method = 'GET';
-        var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
-        var async = true;
+var successCallback = function(position){
+    var x = position.coords.latitude;
+    var y = position.coords.longitude;
+    displayLocation(x,y);
+};
 
-        request.open(method, url, async);
-        request.onreadystatechange = function(){
-          if(request.readyState == 4 && request.status == 200){
-            var data = JSON.parse(request.responseText);
-            var address = data.results[0];
-            var city = data.results[0].address_components[2].long_name;
-            var state = data.results[0].address_components[5].short_name;
-            $("#location").html(city+", "+state);
-          }
-        };
-        request.send();
-      };
+var errorCallback = function(error){
+    var errorMessage = 'Unknown error';
+    switch(error.code) {
+      case 1:
+        errorMessage = 'Permission denied';
+        break;
+      case 2:
+        errorMessage = 'Position unavailable';
+        break;
+      case 3:
+        errorMessage = 'Timeout';
+        break;
+    }
+    document.write(errorMessage);
+};
 
-      var successCallback = function(position){
-        var x = position.coords.latitude;
-        var y = position.coords.longitude;
-        displayLocation(x,y);
-      };
+var options = {
+    enableHighAccuracy: true,
+    timeout: 1000,
+    maximumAge: 0
+};
 
-      var errorCallback = function(error){
-        var errorMessage = 'Unknown error';
-        switch(error.code) {
-          case 1:
-            errorMessage = 'Permission denied';
-            break;
-          case 2:
-            errorMessage = 'Position unavailable';
-            break;
-          case 3:
-            errorMessage = 'Timeout';
-            break;
-        }
-        document.write(errorMessage);
-      };
-
-      var options = {
-        enableHighAccuracy: true,
-        timeout: 1000,
-        maximumAge: 0
-      };
-
-      navigator.geolocation.getCurrentPosition(successCallback,errorCallback,options);
+navigator.geolocation.getCurrentPosition(successCallback,errorCallback,options);
 
